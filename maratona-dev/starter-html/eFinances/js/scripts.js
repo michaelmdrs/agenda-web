9a46f464ca229d02bcc78f8050799a8c3e55097f
Michael Santos <mackelldeveloper@gmail.com>
2021-02-08 23:37:00 -0300

    initial project

--- maratona-dev/starter-html/eFinances/js/scripts.js
+++ maratona-dev/starter-html/eFinances/js/scripts.js
@@ -12,3 +12,17 @@ const Modal = {
         .remove('active')
     }
 }
+
+const transactions = []
+
+const Transaction = {
+    incomes() {
+        // soma a entrada
+    },
+    expenses() {
+        // soma as saídas
+    },
+    total() {
+        // entradas - saídas
+    }
+}
