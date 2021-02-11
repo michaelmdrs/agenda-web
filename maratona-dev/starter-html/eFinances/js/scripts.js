20e92dadb8421575fc7a216c1adcd0bcffa65e59
Michael Santos <mackelldeveloper@gmail.com>
2021-02-11 05:39:54 -0300

    formatando o js com informações do html

--- maratona-dev/starter-html/eFinances/js/scripts.js
+++ maratona-dev/starter-html/eFinances/js/scripts.js
@@ -13,7 +13,26 @@ const Modal = {
     }
 }
 
-const transactions = []
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '10/02/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 250000,
+        date: '20/02/2021',
+    },
+    {
+        id: 3,
+        description: 'Brisanet',
+        amount: -13600,
+        date: '10/02/2021',
+    },
+]
 
 const Transaction = {
     incomes() {
@@ -26,3 +45,57 @@ const Transaction = {
         // entradas - saídas
     }
 }
+// Pegas as transações no objeto aqui no JS e colocar no HTML
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+        const CSSclasses = transaction.amount > 0 ? 'income' :
+ 'expense'
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclasses}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+
+        return html
+    },
+
+    updateBalance() {
+        document.querySelector()
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : ''
+
+        value = String(value).replace(/\D/g, '')
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
