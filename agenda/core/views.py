d72f81b7a27164ce2c00d241d8c3e59621328fd1
Michael <mackelldeveloper@gmail.com>
2022-04-24 10:33:49 -0300

    Criação do projeto e primeira rota

---
+++ agenda/core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+"""
+def hello(request, nome):
+    return HttpResponse(f'<h1>Olá, {nome}!</h1>')
+    
+def adicao(request, num1, num2):
+    resultado = num1 + num2
+    return HttpResponse(f'A soma entre {num1} + {num2} = {resultado}')
+
+def subtracao(request, num1, num2):
+    resultado = num1 - num2
+    return HttpResponse(f'A subtração entre {num1} - {num2} = {resultado}')
+
+def divisao(request, num1, num2):
+    resultado = num1 / num2
+    return HttpResponse(f'A divisão entre {num1} / {num2} = {resultado:.2f}')
+
+def multiplicacao(request, num1, num2):
+    resultado = num1 * num2
+    return HttpResponse(f'A multiplicação entre {num1} * {num2} = {resultado}')
+    
+"""
+
+
+
